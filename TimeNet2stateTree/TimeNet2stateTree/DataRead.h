3f8a20f9c4cfcac0f0c118b31921efaed9cecac8
yi2021 <sijiayi98@163.com>
2021-03-24 13:32:32 +0800

    test 1

--- TimeNet2stateTree/TimeNet2stateTree/DataRead.h
+++ TimeNet2stateTree/TimeNet2stateTree/DataRead.h
@@ -1,8 +1,8 @@
 #pragma once
-#define M0_path  "E:\\xh\\Œ¨”‡ ¶–÷≥Ã–Ú\\Data\\m0.txt"
-#define Delay_path "E:\\xh\\Œ¨”‡ ¶–÷≥Ã–Ú\\Data\\delay.txt"
-#define GoalPlace_path "E:\\xh\\Œ¨”‡ ¶–÷≥Ã–Ú\\Data\\GoalPlace.txt"
-#define GoalMarking_path "E:\\xh\\Œ¨”‡ ¶–÷≥Ã–Ú\\Data\\\GoalMarking.txt"
+#define M0_path  "F:\\github\\T2S\\TimeNet2stateTree\\Data\\m0.txt"
+#define Delay_path "F:\\github\\T2S\\TimeNet2stateTree\\Data\\delay.txt"
+#define GoalPlace_path "F:\\github\\T2S\\TimeNet2stateTree\\Data\\GoalPlace.txt"
+#define GoalMarking_path "F:\\github\\T2S\\TimeNet2stateTree\\Data\\\GoalMarking.txt"
 
 void DataRead(short place_num, short M0[], float Delay[], Tree *Tree);
 void DataRead_v2(short place_num, short M0[], float Delay[], Tree *Tree);
